d49ee96a264f0b594aa1bf5619b0230d1b79183e
faturnangin <sbfatur@gmail.com>
2023-10-15 16:03:52 +0700

    bg color changing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex justify-between">
+    <div className="flex flex-col justify-between bg-cyan-400">
       <h1 className="text-xl font-semibold">NextJS Stackblitz Starter</h1>
       <h1 className="text-xl font-semibold">Nangin</h1>
     </div>
