cc190be3bfed312c287a2af59b363579b1aefaf2
faturnangin <63069254+faturnangin@users.noreply.github.com>
2023-10-15 14:01:31 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="flex justify-between">
+      <h1 className="text-xl font-semibold">NextJS Stackblitz Starter</h1>
+      <h1 className="text-xl font-semibold">Nangin</h1>
+    </div>
+  );
+}
